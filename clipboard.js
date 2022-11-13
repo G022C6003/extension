97e623592bfc51ace063f5e33500b0615ff3b91c
noshiro3 <agopurokun@gmail.com>
2022-11-13 23:59:12 +0900

    20221113--一旦終了

--- clipboard.js
+++ clipboard.js
@@ -2,11 +2,7 @@
 let getStorageLength = 0;
 //選択したエリアを格納する変数(グロ変)
 let activeTextArea;
-
-let sendlocalStorage = {};
-
-let clearButton = document.querySelector('.deleteStorageAll');
-let checkButton = document.querySelector('.checkStorage');
+let insertHtml = document.getElementById('#setClipboard_1');
 
 function sendOk() {
     console.log('送ったよ');
@@ -20,31 +16,9 @@ window.addEventListener("mouseup", () => {
     activeTextArea = window.getSelection().toString();
     if (!activeTextArea) {
     } else {
-        localStorage.setItem(getStorageLength, activeTextArea);
-        getStorageLength++;
         //localStorageの中身を送れるようにする
         //コンテンツスクリプトからバックグラウンドスクリプトに送る
-        console.log(localStorage);
         var sending = browser.runtime.sendMessage(activeTextArea);
         sending.then(sendOk, sendNg);
     }
 });
-
-clearButton.addEventListener("click", () => {
-    //バックグラウンド側のlocalStorage
-    localStorage.clear();
-    console.log(localStorage);
-    getStorageLength = 0;
-});
-
-checkButton.addEventListener("click", () => {
-    console.log(localStorage);
-});
-
-
-
-function getLocalStorage() {
-    for (i = 0; i < getStorageLength; i++) {
-        sendlocalStorage[i] = localStorage.getItem(i);
-    }
-}
